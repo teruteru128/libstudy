47a57626dc062b673a9c68170e1a4d46d9cb952b
Teruteru <teruterubouzu1024@gmail.com>
2020-06-23 18:02:20 +0900

    Update

--- src/study.c
+++ src/study.c
@@ -1,5 +1,14 @@
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include "gettext.h"
+#define _(str) gettext(str)
+#include <locale.h>
+#include <stdio.h>
+
 int study_helloworld(void)
 {
+    printf(_("Hello World!\n"));
     return 0;
 }
