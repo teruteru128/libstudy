441781e213ede36080df49155ce2edbd064ba5aa
Teruteru <teruterubouzu1024@gmail.com>
2020-06-24 00:43:51 +0900

    Add library source files

---
+++ src/queue.c
@@ -0,0 +1,10 @@
+
+#include <stdint.h>
+#include <stdio.h>
+#include "queue.h"
+
+//リングバッファ
+int enq(seedqueue *queue, chunk_pos *pos) {}
+chunk_pos *deq(seedqueue *queue) {}
+int init_queue(seedqueue *queue, size_t size) {}
+int destry_queue(seedqueue *queue) {}
