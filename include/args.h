a316c6f91772bd6173d826aca26358f5cab5c917
teruteru128 <teruterubouzu1024@gmail.com>
2020-06-25 13:59:37 +0900

    Update args

---
+++ include/args.h
@@ -0,0 +1,9 @@
+
+#ifndef ARGS_H
+#define ARGS_H
+
+typedef struct args_t
+{
+} args_t;
+
+#endif
---
+++ include/args_parse.h
@@ -0,0 +1,12 @@
+
+#ifndef ARGS_PARSE_H
+#define ARGS_PARSE_H
+
+#include "args.h"
+
+void parse_args(args_t *args, int argc, char *argv[])
+{
+  return;
+}
+
+#endif
