82a6526245c29ce8ed6ea230a046afabd1434c10
Teruteru <teruterubouzu1024@gmail.com>
2020-07-01 23:10:32 +0900

    Update

--- include/java_random.h
+++ include/java_random.h
@@ -9,17 +9,17 @@ typedef struct random_t Random;
 // Random *Random_constructor();
 // int Random_destructor(Random *);
 // Random *random_new();
-// int random_free(Random *);
-Random *setSeed(Random *, int64_t);
-int32_t next(Random *, int32_t);
-int64_t nextLong(Random *);
-int32_t nextInt(Random *);
-int32_t nextIntWithBounds(Random *, int32_t);
-double nextDouble(Random *);
+// void random_free(Random *);
+Random *setSeed(int64_t *, int64_t);
+int32_t next(int64_t *, int32_t);
+int64_t nextLong(int64_t *);
+int32_t nextInt(int64_t *);
+int32_t nextIntWithBounds(int64_t *, int32_t);
+double nextDouble(int64_t *);
 
 // java.util.Random 内部状態計算ユーティリティ
 int64_t n(int64_t);
 int64_t nInverse(int64_t);
-int64_t initializeSeed(int64_t);
+int64_t initialScramble(int64_t);
 
 #endif
--- src/java_random.c
+++ src/java_random.c
@@ -1,40 +1,38 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "internal_random.h"
 
-static int64_t initialScramble(uint64_t seed)
+int64_t initialScramble(int64_t seed)
 {
     return (seed ^ MULTIPLIER) & MASK;
 }
 
-Random *setSeed(Random *rnd, int64_t seed)
+int64_t *setSeed(int64_t *rnd, int64_t seed)
 {
-    rnd->seed = initialScramble(seed);
+    *rnd = initialScramble(seed);
     return rnd;
 }
 
-int32_t next(Random *rnd, int32_t bits)
+int32_t next(int64_t *rnd, int32_t bits)
 {
-    uint64_t oldseed, nextseed;
-    uint64_t seed = rnd->seed;
-    oldseed = seed;
-    nextseed = (oldseed * MULTIPLIER + ADDEND) & MASK;
-    rnd->seed = nextseed;
-    return (uint32_t)(nextseed >> (48 - bits));
+    int64_t oldseed;
+    oldseed = *rnd;
+    return (int32_t)((*rnd = (oldseed * MULTIPLIER + ADDEND) & MASK) >> (48 - bits));
 }
 
-int64_t nextLong(Random *rnd)
+int64_t nextLong(int64_t *rnd)
 {
     return ((int64_t)(next(rnd, 32)) << 32) + next(rnd, 32);
 }
 
-int32_t nextInt(Random *rnd)
+int32_t nextInt(int64_t *rnd)
 {
     return next(rnd, 32);
 }
 
-int32_t nextIntWithBounds(Random *rnd, int32_t bound)
+int32_t nextIntWithBounds(int64_t *rnd, int32_t bound)
 {
     if (bound <= 0)
     {
@@ -56,7 +54,7 @@ int32_t nextIntWithBounds(Random *rnd, int32_t bound)
     return r;
 }
 
-double nextDouble(Random *rnd)
+double nextDouble(int64_t *rnd)
 {
   return (((int64_t)(next(rnd, 26)) << 27) + next(rnd, 27)) * DOUBLE_UNIT;
 }
@@ -71,8 +69,4 @@ int64_t n(int64_t seed) {
 
 int64_t nInverse(int64_t seed) {
   return (seed - ADDEND) * INVERSE_MULTIPLIER & MASK;
-}
-
-int64_t initializeSeed(int64_t seed) {
-  return (seed ^ MULTIPLIER) & MASK;
 }
