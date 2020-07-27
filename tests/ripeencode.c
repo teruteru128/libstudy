579daa99ab0335db800bfd1a58b40960bae7e820
Teruteru <teruterubouzu1024@gmail.com>
2020-07-28 02:24:42 +0900

    Update

--- tests/ripeencode.c
+++ tests/ripeencode.c
@@ -5,14 +5,14 @@
 #include <changebase.h>
 #include <bm.h>
 
-#define DATA "000111d38e5fc9071ffcd20b4a763cc9ae4f252bb4e48fd66a835e252ada93ff480d6dd43dc62a641155a5"
-#define TARGET "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+#define DATA "00000D9663F57318B4E52288BFDC8B3C23E84DE1"
+#define TARGET "BM-NAxwJoagtHikgoyv2wsDpdFdwb867sKn"
 
 int main(int argc, char const *argv[])
 {
     unsigned char *in = NULL;
     size_t len = parseHex(&in, DATA);
-    char *out = base58encode(in, len);
+    char *out = encodeAddress(4, 1, in, len);
     int r = strcmp(out, TARGET);
     free(in);
     free(out);
