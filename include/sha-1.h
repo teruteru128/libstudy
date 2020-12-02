883b41e511b367a348462d3815e746487201b123
Teruteru <teruterubouzu1024@gmail.com>
2020-12-02 13:42:16 +0900

    Update

--- include/sha-1.h
+++ include/sha-1.h
@@ -2,16 +2,18 @@
 #ifndef SHA_1_H
 #define SHA_1_H
 
-# define SHA_LONG unsigned int
+#define SHA_LONG unsigned int
 
-# define SHA_LBLOCK      16
-# define SHA_CBLOCK      (SHA_LBLOCK*4)/* SHA treats input data as a
-                                        * contiguous array of 32 bit wide
-                                        * big-endian values. */
-# define SHA_LAST_BLOCK  (SHA_CBLOCK-8)
-# define SHA_DIGEST_LENGTH 20
+#define SHA_LBLOCK 16
+/* SHA treats input data as a 
+ * contiguous array of 32 bit wide 
+ * big-endian values. */
+#define SHA_CBLOCK (SHA_LBLOCK * 4)
+#define SHA_LAST_BLOCK (SHA_CBLOCK - 8)
+#define SHA_DIGEST_LENGTH 20
 
-typedef struct SHAstate_st {
+typedef struct SHAstate_st
+{
     SHA_LONG h0, h1, h2, h3, h4;
     SHA_LONG Nl, Nh;
     SHA_LONG data[SHA_LBLOCK];
@@ -19,4 +21,3 @@ typedef struct SHAstate_st {
 } SHA_CTX;
 
 #endif
-
