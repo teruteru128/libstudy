8cd28e515db037fd9182b9df47e16cf02094a01d
Teruteru <teruterubouzu1024@gmail.com>
2021-05-18 17:05:42 +0900

    Update sha

--- src/sha-2.c
+++ src/sha-2.c
@@ -1,12 +1,16 @@
 
-void sha256_init()
-{
-}
+void SHA224_Init(){}
+void SHA224_Update(){}
+void SHA224_Final(){}
 
-void sha256_update()
-{
-}
+void SHA256_Init(){}
+void SHA256_Update(){}
+void SHA256_Final(){}
 
-void sha256_finish()
-{
-}
+void SHA384_Init(){}
+void SHA384_Update(){}
+void SHA384_Final(){}
+
+void SHA512_Init(){}
+void SHA512_Update(){}
+void SHA512_Final(){}
---
+++ src/sha-3.c
@@ -0,0 +1,16 @@
+
+void SHA3_224_Init(){}
+void SHA3_224_Update(){}
+void SHA3_224_Final(){}
+
+void SHA3_256_Init(){}
+void SHA3_256_Update(){}
+void SHA3_256_Final(){}
+
+void SHA3_384_Init(){}
+void SHA3_384_Update(){}
+void SHA3_384_Final(){}
+
+void SHA3_512_Init(){}
+void SHA3_512_Update(){}
+void SHA3_512_Final(){}
