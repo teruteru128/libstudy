c085870f02121751fdf2fcd48ba35d285e033556
Teruteru <teruterubouzu1024@gmail.com>
2021-05-18 23:20:43 +0900

    Fix strjoin

--- src/strjoin.c
+++ src/strjoin.c
@@ -2,19 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Joinを1関数で完結させるのはまずいような？
+// Joinを1関数で完結させるのはまずいような？
 char *strjoin(char *delimiter, char **array, size_t arraylen)
 {
     // delimiterがNULLのときの考慮は？ ->
     // delimiterを別の変数に移し替えてNULLの場合は空文字列にする
-    char *deli = delimiter;
-    if (deli == NULL)
-    {
-        deli = "";
-    }
-    size_t deli_len = strlen(deli);
-    size_t strcap = 0;
-    int i = 0;
+    char *deli = (delimiter == NULL) ? delimiter : NULL;
+    size_t deli_len = (deli != NULL) ? strlen(deli): 0;
     char *rtnstr = NULL;
 
     if (arraylen == 0 || array == NULL)
@@ -22,18 +16,32 @@ char *strjoin(char *delimiter, char **array, size_t arraylen)
         rtnstr = calloc(1, 1);
         return rtnstr;
     }
+    size_t strcap = 0;
+    size_t *arrayitemslen = alloca(arraylen * sizeof(size_t));
     // sum array texts
-    for (i = 0; i < arraylen; i++)
+    for (size_t i = 0; i < arraylen; i++)
     {
-        strcap += strlen(array[i]);
+        arrayitemslen[i] = strlen(array[i]);
+        strcap += arrayitemslen[i];
     }
     // sum used delimiter
-    if (arraylen > 1)
-    {
-        strcap += (arraylen - 1) * deli_len;
-    }
+    // arraylenが0ならば(arraylen - 1) * deli_lenは0なので加算しても変わらない
+    strcap += (arraylen - 1) * deli_len;
     // sum capacity for string termination(\0)
     strcap += 1;
 
     rtnstr = calloc(strcap, sizeof(char));
+    memcpy(rtnstr, array[0], arrayitemslen[0]);
+    size_t pos = arrayitemslen[0];
+    for(size_t i = 1; i < arraylen; i++)
+    {
+        if(deli_len != 0)
+        {
+            memcpy(rtnstr + pos, deli, deli_len);
+            pos += deli_len;
+        }
+        memcpy(rtnstr + pos, array[i], arrayitemslen[i]);
+        pos += arrayitemslen[i];
+    }
+    return rtnstr;
 }
