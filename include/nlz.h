ac3b605fd02ddd8bad63191e650f77e82e48f19d
Teruteru <teruterubouzu1024@gmail.com>
2021-07-18 13:36:40 +0900

    Move getNLZ impl

--- include/nlz.h
+++ include/nlz.h
@@ -9,32 +9,6 @@ for(int i = 0; i < 5; i++)
     flg |= hash[i] != 0;
 
 */
-static size_t getNLZ(const unsigned char *ripe, const size_t limit)
-{
-    size_t nlz = 0;
-    /*
-    unsigned long *ripe_ul = (unsigned long *)ripe;
-    size_t size_ul = limit >> 3;
-    while(ripe_ul[nlz] == 0 && nlz < size_ul)
-    {
-        nlz++;
-    }
-    nlz <<= 3;
-    */
-    /*
-    unsigned int *ripe_u = (unsigned int *)ripe;
-    size_t size_ul = limit >> 2;
-    while(ripe_ul[nlz] == 0 && nlz < size_ul)
-    {
-        nlz++;
-    }
-    nlz <<= 2;
-    */
-    while (ripe[nlz] == 0 && nlz < limit)
-    {
-        nlz++;
-    }
-    return nlz;
-}
+size_t getNLZ(const unsigned char *ripe, const size_t limit);
 
 #endif
--- src/nlz.c
+++ src/nlz.c
@@ -5,3 +5,31 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+size_t getNLZ(const unsigned char *ripe, const size_t limit)
+{
+    size_t nlz = 0;
+    /*
+    unsigned long *ripe_ul = (unsigned long *)ripe;
+    size_t size_ul = limit >> 3;
+    while(ripe_ul[nlz] == 0 && nlz < size_ul)
+    {
+        nlz++;
+    }
+    nlz <<= 3;
+    */
+    /*
+    unsigned int *ripe_u = (unsigned int *)ripe;
+    size_t size_ul = limit >> 2;
+    while(ripe_ul[nlz] == 0 && nlz < size_ul)
+    {
+        nlz++;
+    }
+    nlz <<= 2;
+    */
+    while (ripe[nlz] == 0 && nlz < limit)
+    {
+        nlz++;
+    }
+    return nlz;
+}
