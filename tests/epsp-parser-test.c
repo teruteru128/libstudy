79f30873ba9b1744de702fdda861d4b17844806d
Teruteru <teruterubouzu1024@gmail.com>
2021-12-06 21:20:58 +0900

    Update

--- tests/epsp-parser-test.c
+++ tests/epsp-parser-test.c
@@ -2,17 +2,17 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <epsp-parser.h>
+#include <errno.h>
+#include <limits.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <regex.h>
-#include <errno.h>
-#include <epsp-parser.h>
 #include <string-array.h>
-#include <limits.h>
+#include <string.h>
 
 /*
- * https://p2pquake.github.io/epsp-specifications/epsp-specifications.html
+ *
   https://p2pquake.github.io/epsp-specifications/epsp-specifications.html
   初期化
   mainloop
@@ -21,11 +21,11 @@
  */
 void print_reg_error(int errorcode, regex_t *buf)
 {
-  size_t len = regerror(errorcode, buf, NULL, 0);
-  char *msg = (char *)calloc(len, sizeof(char));
-  regerror(errorcode, buf, msg, len);
-  printf("%s\n", msg);
-  free(msg);
+    size_t len = regerror(errorcode, buf, NULL, 0);
+    char *msg = (char *)calloc(len, sizeof(char));
+    regerror(errorcode, buf, msg, len);
+    printf("%s\n", msg);
+    free(msg);
 }
 
 /*
@@ -33,73 +33,74 @@ void print_reg_error(int errorcode, regex_t *buf)
  */
 int main(int argc, char *argv[])
 {
-  char path[PATH_MAX];
-  snprintf(path, PATH_MAX, "%s/epsp-packet-sample-success.txt", getenv("srcdir"));
-  printf("%d\n", PATH_MAX);
-  printf("%s\n", path);
-  FILE *in = fopen(path, "r");
-  if (!in)
-  {
-    perror("fopen");
-    return EXIT_FAILURE;
-  }
-  char buf[BUFSIZ];
-  printf("%d\n", BUFSIZ);
-  size_t len = 0L;
-  while (fgets(buf, BUFSIZ, in))
-  {
-    fprintf(stdout, "%s", buf);
-  }
-  int r = fclose(in);
-  if (r)
-  {
-    perror("fopen");
-    return EXIT_FAILURE;
-  }
-  /*
-  // 正規表現で分割 OR strtokで分割
-  //
-  char* regex = "([[:digit:]]+) ([[:digit:]]+) ?(.*)?";
-  char* str = "116 12 25:6911:901:2";
-  char** packets={
-    "211",
-    "232",
-    "233",
-    "234",
-    "235",
-    "236",
-    "237",
-    "238",
-    "239",
-    "243",
-    "244",
-    "246",
-    "247",
-    "248",
-    "291",
-    "292",
-    "293",
-    "295",
-    "298",
-    "299",
-    "551",
-    "552",
-    "555",
-    "556",
-    "561",
-    "611",
-    "612",
-    "614",
-    "615",
-    "631",
-    "632",
-    "634",
-    "694",
-    NULL
-  };
-  if(split_by_regex(str, regex) != EXIT_SUCCESS){
-    return EXIT_FAILURE;
-  }
-  */
-  return EXIT_SUCCESS;
+    char path[PATH_MAX];
+    snprintf(path, PATH_MAX, "%s/epsp-packet-sample-success.txt",
+             getenv("srcdir"));
+    printf("%d\n", PATH_MAX);
+    printf("%s\n", path);
+    FILE *in = fopen(path, "r");
+    if (!in)
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+    char buf[BUFSIZ];
+    printf("%d\n", BUFSIZ);
+    size_t len = 0L;
+    while (fgets(buf, BUFSIZ, in))
+    {
+        fprintf(stdout, "%s", buf);
+    }
+    int r = fclose(in);
+    if (r)
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+    /*
+    // 正規表現で分割 OR strtokで分割
+    //
+    char* regex = "([[:digit:]]+) ([[:digit:]]+) ?(.*)?";
+    char* str = "116 12 25:6911:901:2";
+    char** packets={
+      "211",
+      "232",
+      "233",
+      "234",
+      "235",
+      "236",
+      "237",
+      "238",
+      "239",
+      "243",
+      "244",
+      "246",
+      "247",
+      "248",
+      "291",
+      "292",
+      "293",
+      "295",
+      "298",
+      "299",
+      "551",
+      "552",
+      "555",
+      "556",
+      "561",
+      "611",
+      "612",
+      "614",
+      "615",
+      "631",
+      "632",
+      "634",
+      "694",
+      NULL
+    };
+    if(split_by_regex(str, regex) != EXIT_SUCCESS){
+      return EXIT_FAILURE;
+    }
+    */
+    return EXIT_SUCCESS;
 }
