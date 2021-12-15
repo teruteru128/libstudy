3767b1153fa2060512c846928288cb9d5c8c2b18
Teruteru <teruterubouzu1024@gmail.com>
2021-12-15 16:00:44 +0900

    Avoid implementations that cause undefined behavior

--- src/minecraft.c
+++ src/minecraft.c
@@ -3,15 +3,15 @@
 #include <stdint.h>
 #include <java_random.h>
 
-int64_t s(int64_t seed, int32_t chunkX, int32_t chunkZ)
+uint64_t s(uint64_t seed, uint32_t chunkX, uint32_t chunkZ)
 {
     return seed + chunkX * chunkX * 0x4c1906 + chunkX * 0x5ac0db + chunkZ * chunkZ * 0x4307a7L + chunkZ * 0x5f24f ^ 0x3ad8025f;
 }
 
-int isSlimeChunk(int64_t *ctx, int64_t seed, int32_t x, int32_t z)
+int isSlimeChunk(uint64_t *ctx, uint64_t seed, uint32_t x, uint32_t z)
 {
     *ctx = initialScramble(s(seed, x, z));
-    return !nextIntWithBounds(ctx, 10);
+    return !nextIntWithBounds((int64_t *)ctx, 10);
 }
 
 #define MC_CACHE_SIZE 1250
